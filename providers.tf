1af7f9722b7fffbc5a4adaa1b3cd8b597ff603ef
jithin c <jithincotp@gmail.com>
2022-10-25 19:58:18 +0530

    updated aws credentials and key

--- providers.tf
+++ providers.tf
@@ -8,9 +8,7 @@ terraform {
 
 
 provider "aws" {
-  region = "eu-west-3"
-  access_key=var.aws_access_key
-  secret_key=var.aws_secret_key
+  # region = "eu-west-3"
+  # access_key=var.aws_access_key
+  # secret_key=var.aws_secret_key
 }
-
-
