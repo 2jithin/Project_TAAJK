d0452970b763f0409aadd9417d2e99bb26779764
jithin c <jithincotp@gmail.com>
2022-10-26 17:07:38 +0530

    provider tf enabled

--- providers.tf
+++ providers.tf
@@ -15,8 +15,8 @@ terraform {
 }
 
 
-# provider "aws" {
-#   # region = "eu-west-3"
-#   # access_key=var.aws_access_key
-#   # secret_key=var.aws_secret_key
-# }
+provider "aws" {
+  # region = "eu-west-3"
+  # access_key=var.aws_access_key
+  # secret_key=var.aws_secret_key
+}
