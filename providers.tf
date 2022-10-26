59a8eb36000949b6e81cab5e6e039acbcf974d04
jithin c <jithincotp@gmail.com>
2022-10-26 17:00:40 +0530

    Updated

--- providers.tf
+++ providers.tf
@@ -9,17 +9,18 @@ terraform {
     }
   }
   cloud {
-    organization = "REPLACE_ME"
+    organization = "taajk"
 
     workspaces {
-      name = "gh-actions-demo"
+      name = "Project_TAAJK"
+      id = "Project_TAAJK"
     }
   }
 }
 
 
-provider "aws" {
-  # region = "eu-west-3"
-  # access_key=var.aws_access_key
-  # secret_key=var.aws_secret_key
-}
+# provider "aws" {
+#   # region = "eu-west-3"
+#   # access_key=var.aws_access_key
+#   # secret_key=var.aws_secret_key
+# }
