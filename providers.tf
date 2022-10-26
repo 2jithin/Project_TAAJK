07ca86f9b25c85e2c0772092eb0e2292ad556891
jithin c <jithincotp@gmail.com>
2022-10-26 16:08:26 +0530

    Updated Terraform cloud setup

--- providers.tf
+++ providers.tf
@@ -1,14 +1,25 @@
 terraform {
   required_providers {
     aws = {
+      source  = "hashicorp/aws",
       version = "= 3.3.0"
+    }
+    random = {
+      sousource = "hashicorp/random"
+    }
+  }
+  cloud {
+    organization = "REPLACE_ME"
+
+    workspaces {
+      name = "gh-actions-demo"
     }
   }
 }
 
 
-provider "aws" {
-  # region = "eu-west-3"
-  # access_key=var.aws_access_key
-  # secret_key=var.aws_secret_key
-}
+# provider "aws" {
+#   # region = "eu-west-3"
+#   # access_key=var.aws_access_key
+#   # secret_key=var.aws_secret_key
+# }
