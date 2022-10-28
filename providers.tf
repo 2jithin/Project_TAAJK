e927a06e1dbcd127c46ab3093aa9fe90174edaef
jithin c <jithincotp@gmail.com>
2022-10-28 20:32:01 +0530

    Updated region

--- providers.tf
+++ providers.tf
@@ -16,12 +16,12 @@ terraform {
 
 # project_taajk aliases for AWS regions
 provider "aws" {
-  alias  = "us-west-2"
-  region = "us-west-2"
+  alias  = "us-west-1"
+  region = "us-west-1"
 }
 provider "aws" {
-  alias  = "us-east-2"
-  region = "us-east-2"
+  alias  = "us-east-1"
+  region = "us-east-1"
 }
 
 
